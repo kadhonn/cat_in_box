0b13a5fa14901e875d74a2207f841488bdd1f3e7
Matthias Ableidinger <matthias.ableidinger@dynatrace.com>
2024-07-31 09:43:16 +0200

    prettier print

--- src/main/kotlin/at/abl/cat_in_box/Main.kt
+++ src/main/kotlin/at/abl/cat_in_box/Main.kt
@@ -10,10 +10,10 @@ fun runValidation() {
 
     printGameState(gameState)
 //    val myGuess = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-    val myGuess = listOf(2, 2, 3, 4, 5, 6, 7, 7, 9, 9, 8, 7, 6, 5, 4, 3, 2)
+    val myGuess = listOf(2, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 6, 5, 4, 3, 2)
     for (guess in myGuess) {
+        printGuess(guess)
         gameState = gameState.guess(guess)
-        println("guessing $guess")
         printGameState(gameState)
         if (gameState.hasWon()) {
             println("Hurray!")
@@ -21,6 +21,18 @@ fun runValidation() {
         }
     }
 
+}
+
+fun printGuess(guess: Int) {
+    for (i in 1..BOXES_COUNT) {
+        print(" ")
+        if (i == guess) {
+            print("#")
+        } else {
+            print(" ")
+        }
+    }
+    println()
 }
 
 fun printGameState(gameState: GameState) {
